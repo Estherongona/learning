570d1fbf7a0f89747ba7ea2918976abe7304897d
estherongona <hadassahadell@gmail.com>
2020-10-29 18:04:55 +0100

    test2

--- Program.cs
+++ Program.cs
@@ -10,7 +10,10 @@ namespace Esther
     {
         static void Main(string[] args)
         {
-            
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
